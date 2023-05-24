8c38e8c34c34dbeb96cf4855e3a82792cd26602b
williamsimionatto <william.sn0226@gmail.com>
2023-05-24 08:56:51 -0300

    chore: add dependecies

--- database.py
+++ database.py
@@ -1,31 +1,37 @@
-import psycopg2
-import pandas as pd
+# import psycopg2
+# import pandas as pd
 
-def connect():
-    conn = psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user="imobiliaria",
-        password="imobiliaria",
-        port="5432"
-    )
+from dotenv import dotenv_values
 
-    return conn
+config = dotenv_values(".env")
 
-def close(conn):
-    conn.close()
+print(config)
 
-def insert(con, sql):
-  cur = con.cursor()
+# def connect():
+#     conn = psycopg2.connect(
+#         host="localhost",
+#         database="postgres",
+#         user="imobiliaria",
+#         password="imobiliaria",
+#         port="5432"
+#     )
 
-  try:
-    cur.execute(sql)
-    con.commit()
-  except (Exception, psycopg2.DatabaseError) as error:
-    print("Error: %s" % error)
-    con.rollback()
-    cur.close()
-    return 1
+#     return conn
 
-  cur.close()
+# def close(conn):
+#     conn.close()
+
+# def insert(con, sql):
+#   cur = con.cursor()
+
+#   try:
+#     cur.execute(sql)
+#     con.commit()
+#   except (Exception, psycopg2.DatabaseError) as error:
+#     print("Error: %s" % error)
+#     con.rollback()
+#     cur.close()
+#     return 1
+
+#   cur.close()
 
